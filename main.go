61ab01574c4a03479c5969e9f0e53ebaeddb0c84
keryx-io <313514504+keryx-io@users.noreply.github.com>
2026-08-28 16:02:31 +0200

    api: extract mux construction for handler tests

--- main.go
+++ main.go
@@ -69,58 +69,47 @@ func randNum(i int) int {
 	return rng.Intn(i)
 }
 
-func main() {
-	port := flag.String("port", "80", "http listening port")
-	timeoutParameter := flag.String("timeout", "60", "time in seconds after last rotation until kaomoji gets rotated again")
-	kaomojisPath := flag.String("kaomojis", "kaomojis.txt", "path to file with kaomojis")
-	templatePath := flag.String("template", "kaomoji_template.html", "path to HTML template file")
-	guideTemplatePath := flag.String("guide-template", "easter_eggs_template.html", "path to Easter egg guide template file")
-	flag.Parse()
+type rotationTracker struct {
+	mu           sync.Mutex
+	timestamp    int64
+	randomNumber int
+}
 
-	timeout, err := strconv.ParseInt(*timeoutParameter, 10, 0)
-	if err != nil {
-		slog.Error("error while parsing timeout parameter", "error", err)
-		panic(err)
+func newRotationTracker(allk kaomojis) *rotationTracker {
+	return &rotationTracker{
+		timestamp:    time.Now().Unix(),
+		randomNumber: randNum(len(allk.Kaomojis)),
 	}
+}
 
-	slog.Info("parsing template file", "path", *templatePath)
-	tmpl, err := template.ParseFiles(*templatePath)
-	if err != nil {
-		slog.Error("error while parsing template file", "error", err)
-		panic(err)
+func (t *rotationTracker) rotateIfStale(allk kaomojis, timeout int64) (string, int64) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	now := time.Now().Unix()
+	if now-t.timestamp > timeout {
+		t.randomNumber = randNum(len(allk.Kaomojis))
+		t.timestamp = now
+		slog.Info("rotating kaomoji", "new", allk.Kaomojis[t.randomNumber].Kaomoji)
 	}
-	slog.Info("parsing Easter egg guide template", "path", *guideTemplatePath)
-	guideTmpl, err := template.ParseFiles(*guideTemplatePath)
-	if err != nil {
-		slog.Error("error while parsing Easter egg guide template", "error", err)
-		panic(err)
+	remaining := timeout - (now - t.timestamp)
+	if remaining < 0 {
+		remaining = 0
 	}
+	return allk.Kaomojis[t.randomNumber].Kaomoji, remaining
+}
 
-	allk := loadKaomojis(*kaomojisPath)
+func (t *rotationTracker) current(allk kaomojis) string {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	return allk.Kaomojis[t.randomNumber].Kaomoji
+}
 
-	var mu sync.Mutex
-	timestamp := time.Now().Unix()
-	randomNumber := randNum(len(allk.Kaomojis))
-
-	registry := prometheus.NewRegistry()
-	registry.MustRegister(collectors.NewGoCollector(), collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
-	metrics := newHTTPMetrics(registry)
+func newMux(allk kaomojis, timeout int64, tmpl, guideTmpl *template.Template) *http.ServeMux {
+	tracker := newRotationTracker(allk)
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		mu.Lock()
-		if time.Now().Unix()-timestamp > timeout {
-			randomNumber = randNum(len(allk.Kaomojis))
-			timestamp = time.Now().Unix()
-			slog.Info("rotating kaomoji", "new", allk.Kaomojis[randomNumber].Kaomoji)
-		}
-		current := allk.Kaomojis[randomNumber].Kaomoji
-		remaining := timeout - (time.Now().Unix() - timestamp)
-		mu.Unlock()
-
-		if remaining < 0 {
-			remaining = 0
-		}
+		current, remaining := tracker.rotateIfStale(allk, timeout)
 		data := templateData{
 			Kaomoji:          current,
 			RemainingSeconds: remaining,
@@ -138,21 +127,15 @@ func main() {
 	})
 
 	mux.HandleFunc("/raw", func(w http.ResponseWriter, r *http.Request) {
-		mu.Lock()
-		k := allk.Kaomojis[randomNumber].Kaomoji
-		mu.Unlock()
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		if _, err := fmt.Fprintln(w, k); err != nil {
+		if _, err := fmt.Fprintln(w, tracker.current(allk)); err != nil {
 			slog.Error("error writing raw response", "error", err)
 		}
 	})
 
 	mux.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
-		mu.Lock()
-		k := allk.Kaomojis[randomNumber].Kaomoji
-		mu.Unlock()
 		w.Header().Set("Content-Type", "application/json")
-		if err := json.NewEncoder(w).Encode(apiResponse{Kaomoji: k, Total: len(allk.Kaomojis)}); err != nil {
+		if err := json.NewEncoder(w).Encode(apiResponse{Kaomoji: tracker.current(allk), Total: len(allk.Kaomojis)}); err != nil {
 			slog.Error("error encoding api response", "error", err)
 		}
 	})
@@ -182,6 +165,44 @@ func main() {
 	mux.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
 		banner(w)
 	})
+
+	return mux
+}
+
+func main() {
+	port := flag.String("port", "80", "http listening port")
+	timeoutParameter := flag.String("timeout", "60", "time in seconds after last rotation until kaomoji gets rotated again")
+	kaomojisPath := flag.String("kaomojis", "kaomojis.txt", "path to file with kaomojis")
+	templatePath := flag.String("template", "kaomoji_template.html", "path to HTML template file")
+	guideTemplatePath := flag.String("guide-template", "easter_eggs_template.html", "path to Easter egg guide template file")
+	flag.Parse()
+
+	timeout, err := strconv.ParseInt(*timeoutParameter, 10, 0)
+	if err != nil {
+		slog.Error("error while parsing timeout parameter", "error", err)
+		panic(err)
+	}
+
+	slog.Info("parsing template file", "path", *templatePath)
+	tmpl, err := template.ParseFiles(*templatePath)
+	if err != nil {
+		slog.Error("error while parsing template file", "error", err)
+		panic(err)
+	}
+	slog.Info("parsing Easter egg guide template", "path", *guideTemplatePath)
+	guideTmpl, err := template.ParseFiles(*guideTemplatePath)
+	if err != nil {
+		slog.Error("error while parsing Easter egg guide template", "error", err)
+		panic(err)
+	}
+
+	allk := loadKaomojis(*kaomojisPath)
+
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(collectors.NewGoCollector(), collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
+	metrics := newHTTPMetrics(registry)
+
+	mux := newMux(allk, timeout, tmpl, guideTmpl)
 	mux.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
 
 	slog.Info("webserver starting", "port", *port)
